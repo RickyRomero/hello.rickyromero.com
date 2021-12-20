1bfca215bf47f619552d0a80079842384cfcd6e9
Ricky Romero <ricky.romero@gmail.com>
2021-12-19 21:34:27 -0800

    Configure eslint

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+const App = ({ Component, pageProps }) => (
+  <Component {...pageProps} />
+)
 
-export default MyApp
+export default App
