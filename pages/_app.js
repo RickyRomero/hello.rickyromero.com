42dc80e6d525d302782a42056d1abe8303acc596
Ricky Romero <ricky.romero@gmail.com>
2021-12-23 22:04:03 -0800

    Detect system a11y settings

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,12 @@
+import { SettingsDetector } from 'components/settings'
+
 import 'styles/_variables.css'
 import 'styles/_globals.css'
 
 const App = ({ Component, pageProps }) => (
-  <Component {...pageProps} />
+  <SettingsDetector>
+    <Component {...pageProps} />
+  </SettingsDetector>
 )
 
 export default App
