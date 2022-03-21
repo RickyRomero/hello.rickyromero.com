9d272ca2e229bf2c2bd1c6220749fae552178373
Ricky Romero <ricky.romero@gmail.com>
2022-03-20 17:39:22 -0700

    Start adding styling to skills cloud

--- components/skills-cloud.js
+++ components/skills-cloud.js
@@ -1,17 +1,35 @@
 import { useEffect, useState } from 'react'
 import { motion } from 'framer-motion'
 
+import cl from 'utils/classlist'
 import skills from './skills-cloud.json'
+import styles from './skills-cloud.module.css'
 
-const SkillsCloud = () => {
-  const [activeSkillset, setActiveSkillset] = useState('design')
+const strengthMap = new Map()
+strengthMap.set(1, 'Basic')
+strengthMap.set(2, 'Advanced')
+strengthMap.set(3, 'Fluent')
+
+const Tooltip = ({ level, details }) => {
+  return (
+    <div className={styles.tooltip}>
+      <div><span>{strengthMap.get(level)}</span></div>
+      <p>{details}</p>
+    </div>
+  )
+}
+
+const SkillsCloud = ({ className }) => {
+  const [activeSkillInfo, setActiveSkillInfo] = useState([null, null])
+  const [activeSet, activeSkill] = activeSkillInfo
+  const { level, details } = activeSet ? skills[activeSet][activeSkill] : {}
 
   const container = {
     hidden: { opacity: 0 },
     show: {
       opacity: 1,
       transition: {
-        staggerChildren: 0.2
+        staggerChildren: 0.0333
       }
     }
   }
@@ -21,27 +39,25 @@ const SkillsCloud = () => {
     show: { opacity: 1 }
   }
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      const skillsetNames = Object.keys(skills)
-      const activeIndex = skillsetNames.indexOf(activeSkillset)
-      const nextIndex = (activeIndex + 1) % skillsetNames.length
-
-      setActiveSkillset(skillsetNames[nextIndex])
-    }, 8000)
-
-    return () => clearInterval(interval)
-  }, [])
+  const updateActiveSkillInfo = (set, skill) => {
+    setActiveSkillInfo([set, skill])
+  }
 
   return (
-    <motion.ul variants={container} initial="hidden" animate="show" exit="hidden">
-      {Object.keys(skills[activeSkillset]).map(skill => (
-        <motion.li
-          key={`${activeSkillset}-${skill}`}
-          variants={item}
-        >
-          {skill}
-        </motion.li>
+    <motion.ul className={cl(styles.skillCloud, className)} variants={container} initial="hidden" animate="show" exit="hidden">
+      {Object.keys(skills).map(set => (
+        Object.keys(skills[set]).map(skill => (
+          <motion.li
+            className={styles.skill}
+            onPointerOver={() => updateActiveSkillInfo(set, skill)}
+            onPointerLeave={() => updateActiveSkillInfo(null, null)}
+            key={`${set}-${skill}`}
+            variants={item}
+          >
+            {skill === activeSkill && <Tooltip {...{ level, details }} />}
+            {skill}
+          </motion.li>
+        ))
       ))}
     </motion.ul>
   )
